d5db34d24dbf1ef9d2d96277b3cd0a3b664495aa
dmoneygetstax <37202382+dmoneygetstax@users.noreply.github.com>
2020-02-01 14:11:50 -0500

    lab3

--- Lab3/index.js
+++ Lab3/index.js
@@ -2,13 +2,13 @@ const Express = require("express");
 const App = Express();
 const port = 80;
 
+App.get("/", function(req, res){
+        res.send("Hello World!");
+    });
+
 App.use(Express.static("public"));
 
 App.use("/public", Express.static("public"));
-
-App.get("/", function(req, res){
-    res.send("Hello World!");
-});
 
 App.listen(port, () => {
     console.log("Server running!");
