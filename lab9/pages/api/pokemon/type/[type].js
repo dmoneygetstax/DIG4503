d2c454ce6acd7702b4291feaba78effee7f5f924
dmoneygetstax <37202382+dmoneygetstax@users.noreply.github.com>
2020-03-30 23:20:19 -0400

    typesearch take 50000

--- lab9/pages/api/pokemon/type/[type].js
+++ lab9/pages/api/pokemon/type/[type].js
@@ -1,26 +1,22 @@
 import getPokemon from 'json-pokemon/getPokemon';
 
 function response(req, res){
+
+    let result = {"error": "Could not find!"};
     
     for(let i=0; i < getPokemon.length; i++) {
 
         for(let k=0; pokemon[i].typeList.length; k++) {
             
+            if(pokemon[k] == req.params.type) {
+                result = pokemon[k];
+            }
 
             }
 
         }
 
-        let result = {"error": "Could not find type."};
-
-        let pokemonArray = pokemon[k](req.query.type);
-
-        if(pokemonArray !== null) {
-            result = pokemonArray;
-        }
-        console.log(pokemon);
         res.json(result);
     }
 
 export default response;
-
